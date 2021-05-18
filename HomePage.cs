c43312ec8a092ab65602bd963ffbf1c29a2db954
lshpylova <gargantua29031990>
2021-05-18 11:53:09 +0300

    Add project files.

---
+++ HomePage.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SwagLabsTests
+{
+    [TestFixture]
+
+    public class HomePage
+    {
+
+        private IWebDriver driver;
+
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void TestLogin()
+        {
+            driver.Url = "https://www.saucedemo.com/";
+            System.Threading.Thread.Sleep(2000);
+            LogInPage loginPage = new LogInPage(driver);
+            loginPage.loginValidUser("standard_user", "secret_sauce");
+
+            /* Perform wait to check the output */
+            System.Threading.Thread.Sleep(2000);
+            Assert.That("PRODUCTS", Is.EqualTo(loginPage.displayName()));
+        }
+
+        [TearDown]
+        public void close_Browser()
+        {
+            driver.Quit();
+        }
+    }
+}
+
+
+
---
+++ LogInPage.cs
@@ -0,0 +1,59 @@
+﻿using OpenQA.Selenium;
+using System;
+
+
+namespace SwagLabsTests
+{
+    class LogInPage
+    {
+        protected IWebDriver driver;
+        By usernameLocator = By.CssSelector("#user-name");
+        By passwordLocator = By.CssSelector("#password");
+        By loginButtonLocator = By.CssSelector("#login-button");
+        By displayNameLocator => By.XPath("//span[text()='Products']");
+
+        public LogInPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public LogInPage typeUsername(String username)
+        {
+            driver.FindElement(usernameLocator).SendKeys(username);
+            return this;
+        }
+
+        public LogInPage typePassword(String password)
+        {
+            driver.FindElement(passwordLocator).SendKeys(password);
+            return this;
+        }
+
+        public HomePage submitLogin()
+        {
+            driver.FindElement(loginButtonLocator).Submit();
+            return new HomePage();
+        }
+
+        public string displayName()
+        {
+            var productTitle=driver.FindElement(displayNameLocator).Text;
+            return productTitle;
+        }
+
+        public HomePage loginAs(String username, String password)
+        {
+            typeUsername(username);
+            typePassword(password);
+            return submitLogin();
+        }
+
+        public HomePage loginValidUser(String userName, String password)
+        {
+            driver.FindElement(usernameLocator).SendKeys(userName);
+            driver.FindElement(passwordLocator).SendKeys(password);
+            driver.FindElement(loginButtonLocator).Click();
+            return new HomePage();
+        }
+    }
+}
